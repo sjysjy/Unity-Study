ad4dea55fe536ddaadea988e7cbce1d89db0ea82
Juyeon Song <jysong0805@naver.com>
2022-02-19 00:29:47 +0900

    B0~B6
    유니티 나를 화나게 해..

---
+++ Gold Metal Basic 3D Game/Assets/LifeCycle.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LifeCycle : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+    private void Update()
+    {
+        Vector3 vec = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+        transform.Translate(vec);
+    }
+}
