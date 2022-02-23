9b808e504206a05bf5540ecb7ff3acef7bb6948d
Juyeon Song <jysong0805@naver.com>
2022-02-23 16:51:34 +0900

    B12

---
+++ Gold Metal Basic 3D Game/Assets/MyBall.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyBall : MonoBehaviour
+{
+    Rigidbody rigid;
+
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody>();
+        
+    }
+
+    void FixedUpdate()
+    {
+       /*  if (Input.GetButtonDown("Jump"))
+         {
+             rigid.AddForce(Vector3.up * 50, ForceMode.Impulse);
+             Debug.Log(rigid.velocity);      //AddForce는 많이 누를수록 속도 velocity가 계속 증가
+         }
+       */
+        Vector3 vec = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+        rigid.AddForce(vec, ForceMode.Impulse);
+
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.name == "Cube")
+            rigid.AddForce(Vector3.up * 2, ForceMode.Impulse);
+    }
+}
---
+++ Gold Metal Basic 3D Game/Assets/OtherBall.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OtherBall : MonoBehaviour
+{
+    MeshRenderer mesh;
+    Material mat;
+    void Start()
+    {
+        mesh = GetComponent<MeshRenderer>();
+        mat = mesh.material;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "My Ball")
+            mat.color = new Color(0, 0, 0);
+        
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.name == "My Ball")
+            mat.color = new Color(1, 1, 1);
+
+    }
+
+}
